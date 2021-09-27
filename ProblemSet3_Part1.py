76bc27a2d39b1fa1e3622234c12b9b0c16c3dbde
mtm34 <89397439+mtm34@users.noreply.github.com>
2021-09-27 09:29:19 -0400

    Initial Problem Set 3 files commit

---
+++ ProblemSet3_Part1.py
@@ -0,0 +1,15 @@
+# %% PS3: Code Block 1
+
+mountain = "Denali"
+nickname = 'Mt. McKinley'
+elevation = 20322 
+
+print (f"{mountain}, formerly\nknown as {nickname}\nis {str(elevation)} above sea level.")
+
+# %% Task 2
+
+#Assign variables
+dataFolder = "W:\859_data\tri_state"
+dataList = ["roads.shp", "road_types.dbf", "naip_imagery.tif"]
+userItem = "streams.shp"
+
