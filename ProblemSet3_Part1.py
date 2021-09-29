8889e7813b77f37241bccf5fdcb05e086f0a1fb1
mtm34 <89397439+mtm34@users.noreply.github.com>
2021-09-29 09:01:00 -0400

    Completed task 3

--- ProblemSet3_Part1.py
+++ ProblemSet3_Part1.py
@@ -19,3 +19,33 @@ dataList.append(userItem)
 #Iterate through dataList
 for x in dataList:
     print(f"{dataFolder}\{x}")
+
+# %% Task 3
+
+#Make an empty list
+userNumbers = []
+
+#Iterate 
+for x in range(3) :
+    x = input("Enter an integer:")
+    userNumbers.append(int(x))
+
+#Sort ascending
+userNumbers.sort()
+#print highest value
+print(f"The highest number is: {userNumbers[-1]}")
+
+# %% Task 3 Challenge
+
+#Make an empty list
+userNumbers = []
+
+#Iterate 
+for x in range(3) :
+    x = input("Enter an integer:")
+    userNumbers.append(int(x))
+
+#Sort descending
+userNumbers.sort(reverse=True)
+#print list
+print(userNumbers)
