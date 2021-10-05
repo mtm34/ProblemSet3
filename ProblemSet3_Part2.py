4de14e85b1399a531e81bab3e517e7ad03ca3412
mtm34 <mtm34@duke.edu>
2021-10-05 15:24:49 -0400

    Fixed issues after meeting with John

--- ProblemSet3_Part2.py
+++ ProblemSet3_Part2.py
@@ -64,60 +64,46 @@ loit_line_list = loit_file_obj.readlines()
 #Release the link to the file objects 
 loit_file_obj.close() 
 
-#Empty dictionaries for latitudes and long
-mmsi_lat = {}
-mmsi_long = {}
+#Save the contents of the first line
+header_line_String = loit_line_list[0]
+
+
+#Split the headerLineString into a list of header items
+header_items = header_line_String.split(",")
+
+#List the index of the mmsi, shipname, and fleet_name values
+mmsiIdx = header_items.index('transshipment_mmsi')
+starting_lat_idx = header_items.index('starting_latitude')
+starting_long_idx = header_items.index('starting_longitude')
+ending_lat_idx = header_items.index("ending_latitude")
+
+#Create empty list for mmsis that match criteria 
+matching_mmsi=[]
 
 #Iterate through lines
-for line in loit_line_list[1:]:
+for line in loit_line_list[1:]: #will iterate through lines, skipping first header line
     #split string into a list of data items
     loit_data = line.split(",")
     #assign variables
-    mmsi =  loit_data[0]  
-    starting_lat = loit_data[1]  
-    ending_lat = loit_data[3]
-    starting_long = loit_data[2]
-    #add to mmsi latitude dictionary 
-    mmsi_lat[mmsi]= (starting_lat, ending_lat)
-    #add to mmsi longitude dictionary
-    mmsi_long[mmsi]= starting_long
- 
-#Create empty list for equator crossing keys
-keys_equator_crossing = []
-#Assess starting and ending latitude for equator crossing
-for mmsi_item in mmsi_lat.items():
-    #get the coordinates
-    mmsi, coordinates = mmsi_item
-    starting_lat, ending_lat = coordinates
-    #see if crosses equator, if starting lat starts with a - and the ending lat doesn't, it should cross equator
-    if starting_lat[0] == "-" and ending_lat[0] != "-":
-        keys_equator_crossing.append(mmsi)
-
-#Create empty list for keys matching longitude criteria
-keys_long_criteria = []
-#Assess starting and ending latitude for equator crossing
-for mmsi_long_item in mmsi_long.items():
-    #get the coordinates
-    mmsi, starting_long = mmsi_long_item
-    int_long = float(starting_long)
-    #see if qualifies longitude criteria
-    if 165< int_long <170:
-        keys_long_criteria.append(mmsi)
-        
-#Convert criteria key lists into sets
-equator_set = set(keys_equator_crossing)
-long_set = set(keys_long_criteria)
-#Create list from intersection
-matching_keys = list(equator_set.intersection(long_set))
+    mmsi =  loit_data[mmsiIdx]  
+    starting_lat = loit_data[starting_lat_idx]  
+    ending_lat = loit_data[ending_lat_idx]
+    starting_long = loit_data[starting_long_idx]
+    #If the vessel meets all the requirements, add MMSI to a list
+    if starting_lat[0]=="-" and ending_lat[0]!= "-" and 165< float(starting_long) <170:
+       matching_mmsi.append(mmsi)
 
 #If no records found, tell user
-if len(matching_keys) == 0:
+if len(matching_mmsi) == 0:
     print ("No Records Found")
     
-#Pull from vessel dict using matching keys
-for matching_key in matching_keys:
-    vesselID = matching_key
-    vesselFleet = vesselDict[matching_key]
-    print(f"Vessel # {vesselID} flies the flag of {vesselFleet}")
+#Print vessels that match criteria
+for key in matching_mmsi:
+    vesselID = key
+    vesselFleet = vesselDict[key] #uses matching key to pull vessel fleet number from task 4
+    print(f"Vessel # {vesselID} flies the flag of {vesselFleet}")       
+
+
+
     
 
